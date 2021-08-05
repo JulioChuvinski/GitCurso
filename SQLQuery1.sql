146faa70499568fd799a5df707e87146320529dd
JulioChuvinski <jcchuvinski@hotmail.com>
2021-08-05 10:45:37 -0300

    Improving the database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,10 @@
-describe pessoas;
+create table pessoas (
+id int not null identity,
+nome varchar(50) not null,
+nascimento date,
+sexo varchar(1) not null check (sexo IN('M','F')),
+peso decimal(5,2),
+altura decimal(3,2),
+nacionalidade varchar(20) default 'Brasil',
+primary key (id)
+);
