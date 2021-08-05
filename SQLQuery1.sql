22baed5de2010d96409735ff9d45693abddde564
JulioChuvinski <jcchuvinski@hotmail.com>
2021-08-05 11:35:54 -0300

    Update
    Add Column Profissão

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,2 @@
-insert into pessoas
-(nome, nascimento, sexo, peso, altura, nacionalidade)
-values
-('Isaac', '2020-09-25', 'M', '12', '0.30', 'italia'),
-('Fabricio', '1880-09-25', 'M', '89', '1.30', 'senegal'),
-('bruna', '1995-01-09', 'F', '80', '1.60', ''),
-('Epaminondas', '1890-04-15', 'F', '60', '1.80', 'Franca');
+alter table pessoas
+add  profissao varchar (10);
