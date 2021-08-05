aa066ff5b79e6e825f4c3bf6711bc5a0a16fb8db
JulioChuvinski <jcchuvinski@hotmail.com>
2021-08-05 11:30:47 -0300

    Update
    Insert more Values in table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,7 @@
 insert into pessoas
 (nome, nascimento, sexo, peso, altura, nacionalidade)
 values
-('Davi', '2019-02-24', 'M', '14', '0.50', 'espanha');
+('Isaac', '2020-09-25', 'M', '12', '0.30', 'italia'),
+('Fabricio', '1880-09-25', 'M', '89', '1.30', 'senegal'),
+('bruna', '1995-01-09', 'F', '80', '1.60', ''),
+('Epaminondas', '1890-04-15', 'F', '60', '1.80', 'Franca');
